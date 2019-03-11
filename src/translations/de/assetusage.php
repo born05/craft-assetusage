9e798ecc5a871367bfd0d947619d79177a88ee4f
Karsten Schempp <Bruellmuecke112@users.noreply.github.com>
2019-03-11 17:28:02 +0100

    Add german translation and update array of strings in en translation file

---
+++ src/translations/de/assetusage.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Asset Usage plugin loaded' => 'Asset Usage plugin wurde geladen.',
+    'Usage' => 'Verwendung',
+    'Used {count} time' => '{count}x verwendet',
+    'Used {count} times' => '{count}x verwendet',
+    'Unused' => 'Ungenutzt',
+];
